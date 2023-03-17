9184b1431b90dffd2668f46e8b19dfc0670d2a3b
Jon Schwartz <jpschwartz2@uwalumni.com>
2023-03-16 22:30:02 -0700

    Simple deploy

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "fevmate/token/WFIL.sol";
 
-contract CounterScript is Script {
+contract WFILScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        WFIL wFIL = new WFIL();
     }
 }
